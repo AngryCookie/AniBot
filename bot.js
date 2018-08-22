e9483d24f78f0eb8fa42dec512dad65327408c87
AngryCookie <36591462+AngryCookie@users.noreply.github.com>
2018-08-22 13:57:06 +0300

    mute

--- bot.js
+++ bot.js
@@ -1,5 +1,15 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
-
+client.on('message', (message) => {
+    let prefix = '.';
+    if(message.content.indexOf(prefix) !== 0) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    if (command == 'mute') {
+        let member = message.mentions.members.first();
+        if (!member) return;
+        member.addRole('id роли мута');
+    }
+});
 
 client.login(process.env.TOKEN)
